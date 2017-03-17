450515a8534f170cd5dc37cc37b2277f5e11931d
xuxianmei <to_changer@126.com>
2017-03-17 16:42:40 +0800

    c

--- java/src/firstJavaProgram/src/com/tlxxm/first/HelloWorld.java
+++ java/src/firstJavaProgram/src/com/tlxxm/first/HelloWorld.java
@@ -1,9 +1,9 @@
 package com.tlxxm.first;
 
 import java.util.Date;
-
+//类名所有单词首字母大写
 public class HelloWorld {
-
+//成员变量和方法的 首个单词的首字母小写，后续单词首字母大写
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello World ! " +new Date());
