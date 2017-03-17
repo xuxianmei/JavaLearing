ade19d5c9a0925482001b1498fb11dca528c066d
xuxianmei <to_changer@126.com>
2017-03-17 16:26:32 +0800

    ignore

--- java/src/firstJavaProgram/src/com/tlxxm/first/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.tlxxm.first;
-import java.util.*;
-public class HelloWorld {
-
-    public static void main(String[] args) {
-	 System.out.println("Hello World! "+new Date());
-    }
-}
---
+++ java/src/firstJavaProgram/src/com/tlxxm/first/Main.java
@@ -0,0 +1,8 @@
+package com.tlxxm.first;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
