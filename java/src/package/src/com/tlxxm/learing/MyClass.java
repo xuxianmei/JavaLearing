93dbb1fcc47cc08ef966ad614041b998aefd6609
xuxianmei <to_changer@126.com>
2017-03-20 15:01:37 +0800

    public private protected

---
+++ java/src/package/src/com/tlxxm/learing/MyClass.java
@@ -0,0 +1,10 @@
+package com.tlxxm.learing;
+
+import com.tlxxm.tool.*;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        Util u= new Util();
+    }
+}
---
+++ java/src/package/src/com/tlxxm/tool/Util.java
@@ -0,0 +1,8 @@
+package com.tlxxm.tool;
+
+
+public class Util {
+    public Util(){
+        System.out.print("com.tlxxm.tool.Util");
+    }
+}
